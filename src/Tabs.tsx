e87874399c1f831c53af2c572b393050c570d188
Anton Vlasov <nanostet@gmail.com>
2022-01-01 14:08:42 +0700

    Format Tabs

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,46 +1,48 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import React from 'react';
-import {StyleSheet, View, Dimensions} from 'react-native';
-import ProfileScreen from './screens/ProfileScreen';
-import TodayScreen from './screens/TodayScreen';
-import CalendarScreen from './screens/CalendarScreen';
-import {NavigationContainer} from '@react-navigation/native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { StyleSheet, View, Dimensions } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
-const Tab = createBottomTabNavigator();
-Ionicons.loadFont();
+import CalendarScreen from './screens/CalendarScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import TodayScreen from './screens/TodayScreen'
+
+const Tab = createBottomTabNavigator()
+Ionicons.loadFont()
 const Tabs = () => {
   return (
     <View style={styles.container}>
       <NavigationContainer>
         <Tab.Navigator
           initialRouteName="Calendar"
-          screenOptions={({route}: any) => ({
+          screenOptions={({ route }: any) => ({
             headerShown: false,
-            tabBarIcon: ({focused, size, color}) => {
-              let iconName;
+            tabBarIcon: ({ focused, size, color }) => {
+              let iconName
               if (route.name === 'Today') {
-                iconName = focused ? 'flower' : 'flower-outline';
+                iconName = focused ? 'flower' : 'flower-outline'
               } else if (route.name === 'Profile') {
-                iconName = focused ? 'person' : 'person-outline';
+                iconName = focused ? 'person' : 'person-outline'
               } else if (route.name === 'Calendar') {
-                iconName = focused ? 'calendar' : 'calendar-outline';
+                iconName = focused ? 'calendar' : 'calendar-outline'
               }
-              return <Ionicons name={iconName} size={size} color={color} />;
+              return <Ionicons color={color} name={iconName} size={size} />
             },
             tabBarOptions: {
               tabBarActiveTintColor: 'tomato',
               tabBarInactiveTintColor: 'grey',
             },
-          })}>
-          <Tab.Screen name="Today" component={TodayScreen} />
-          <Tab.Screen name="Calendar" component={CalendarScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
+          })}
+        >
+          <Tab.Screen component={TodayScreen} name="Today" />
+          <Tab.Screen component={CalendarScreen} name="Calendar" />
+          <Tab.Screen component={ProfileScreen} name="Profile" />
         </Tab.Navigator>
       </NavigationContainer>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -49,5 +51,5 @@ const styles = StyleSheet.create({
     // borderWidth:5,
     height: Dimensions.get('window').height * 0.9,
   },
-});
-export default Tabs;
+})
+export default Tabs
