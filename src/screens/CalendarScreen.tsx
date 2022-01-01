6a661d084b6ba85ca94b3acac9e0c773dcd7d6c3
glushkina1 <40953265+glushkina1@users.noreply.github.com>
2022-01-02 00:31:53 +0300

    fixed styles1

--- src/screens/CalendarScreen.tsx
+++ src/screens/CalendarScreen.tsx
@@ -1,39 +1,44 @@
-import { format } from 'date-fns'
-import React, { useState } from 'react'
-import { Dimensions, SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import { Calendar } from 'react-native-calendars'
-
-import CalendarHeader from '../CalendarHeader'
+/* eslint-disable */
+import React, {useState} from 'react';
+import {Dimensions, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Calendar} from 'react-native-calendars';
+import CalendarHeader from '../CalendarHeader';
+import {format} from 'date-fns';
 
 const CalendarScreen = () => {
-  const [currentDate, setCurrentDate] = useState(new Date())
-  const [showJumpToday, setShowJumpToday] = useState(false)
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [showJumpToday, setShowJumpToday] = useState(false);
 
-  const dateFormatter = (currentDate: Date) => {
-    return format(currentDate, 'yyyy-MM')
-  }
+  let dateFormatter = (currentDate: Date) => {
+    return format(currentDate, 'yyyy-MM');
+  };
 
   const CalendarHeaderHandler = () => {
-    return CalendarHeader(currentDate, setCurrentDate, showJumpToday, setShowJumpToday)
-  }
+    return CalendarHeader(
+      currentDate,
+      setCurrentDate,
+      showJumpToday,
+      setShowJumpToday,
+    );
+  };
 
   return (
     <SafeAreaView style={styles.screenContainer}>
-      <View style={{ ...styles.calendarContainer, ...styles.itemContainer }}>
+      <View style={{...styles.calendarContainer, ...styles.itemContainer}}>
         <Calendar
           current={dateFormatter(currentDate)}
-          customHeader={CalendarHeaderHandler}
-          firstDay={1}
           key={dateFormatter(currentDate)}
+          customHeader={CalendarHeaderHandler}
           style={styles.calendar}
+          firstDay={1}
         />
       </View>
-      <View style={{ ...styles.infoContainer, ...styles.itemContainer }}>
+      <View style={{...styles.infoContainer, ...styles.itemContainer}}>
         <Text style={styles.infoText}>INFO</Text>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   screenContainer: {
